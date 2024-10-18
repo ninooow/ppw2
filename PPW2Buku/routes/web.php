af696d4e98c2391d9e966dd31145191c2507b12d
Nino Auliya Nahara <ninoauliyanahara@gmail.com>
2024-10-18 22:15:03 +0700

    ubah nama folder

---
+++ PPW2Buku/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\BukuController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/buku',[BukuController::class,'index']);
+Route::get( '/buku/create' , [BukuController::class,'create'])->name('buku.create'); //get buat memanggil saja
+Route::post('/buku', [BukuController::class,'store'])->name('buku.store'); //post buat ngirim data
+Route::delete('/buku/{id}', [BukuController::class,'destroy'])->name('buku.destroy');
+Route::get('/buku/{id}/edit',[BukuController::class,'edit'])->name('buku.edit');
+Route::put('/buku/{id}',[BukuController::class,'update'])->name('buku.update');
+Route::get('/buku/search',[BukuController::class, 'search'])->name('buku.search');
