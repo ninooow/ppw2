1510f94bb7fe937424614f19683114f510908f4d
Nino Auliya Nahara <ninoauliyanahara@gmail.com>
2024-10-28 22:57:15 +0700

    commit to pertemuan9 and master

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginRegisterController;
 use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', function() {
+    return view('welcome');
+});
 Route::get('/buku',[BukuController::class,'index']);
 Route::get( '/buku/create' , [BukuController::class,'create'])->name('buku.create'); //get buat memanggil saja
 Route::post('/buku', [BukuController::class,'store'])->name('buku.store'); //post buat ngirim data
@@ -11,3 +14,12 @@ Route::delete('/buku/{id}', [BukuController::class,'destroy'])->name('buku.destr
 Route::get('/buku/{id}/edit',[BukuController::class,'edit'])->name('buku.edit');
 Route::put('/buku/{id}',[BukuController::class,'update'])->name('buku.update');
 Route::get('/buku/search',[BukuController::class, 'search'])->name('buku.search');
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
